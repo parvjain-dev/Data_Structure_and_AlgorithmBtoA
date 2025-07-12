174d9b9463e8fcc39a170bb7d1963a8644a491d3
Parv Jain <parvjain12@protonmail.com>
2025-07-12 21:08:28 +0530

    Jai shri ram

---
+++ basics.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main (){
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+
+    cout<<"Your number is : "<<a;
+    
+    return 0;
+}
